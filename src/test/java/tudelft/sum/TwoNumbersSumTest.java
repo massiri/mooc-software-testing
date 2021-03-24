0027047eb81eeda5569e9e0a80532ae1cb0317c8
massiri <39437055+massiri@users.noreply.github.com>
2021-03-24 18:36:04 +0100

    Practical exercise #1

--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -36,4 +36,18 @@ public class TwoNumbersSumTest {
         Assertions.assertEquals(test2 , test1);
     }
 
+    @Test
+    public void sameLenghDiffResut() {
+        TwoNumbersSum result = new TwoNumbersSum();
+        ArrayList<Integer> l1 = new ArrayList<Integer>();
+        ArrayList<Integer> l2 = new ArrayList<Integer> ();
+        ArrayList<Integer> test1 = new ArrayList<Integer> ();
+        ArrayList<Integer> test2 = new ArrayList<Integer> ();
+        l1.add(1);l1.add(0);l1.add(3);
+        l2.add(2);l2.add(2);l2.add(8);
+        test2.add(1);test2.add(1);test2.add(2);test2.add(3);
+        test1 = result.addTwoNumbers(l1,l2);
+        Assertions.assertEquals(test2 , test1);
+    }
+
 }
